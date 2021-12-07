33a82262f72ccd13c508933199260fe2da583de7
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-12-07 16:58:51 +0300

    Update variables.py

--- common/variables.py
+++ common/variables.py
@@ -1,3 +1,5 @@
+import logging
+
 """Константы"""
 
 # Порт поумолчанию для сетевого ваимодействия
@@ -10,6 +12,8 @@ MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_LEVEL = logging.DEBUG
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
