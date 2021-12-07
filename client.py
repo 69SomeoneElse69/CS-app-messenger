dd634c0af6d0bbc166a1b4f72edf35d4a06dca89
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-12-07 16:56:03 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,17 +4,26 @@ import sys
 import json
 import socket
 import time
-from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+import argparse
+import logging
+import logs.config_client_log
+from common.variables import ACTION, TIME, USER, ACCOUNT_NAME, RESPONSE, \
+    DEFAULT_IP_ADDRESS, DEFAULT_PORT, ERROR, PRESENCE
 from common.utils import message_in, message_out
+from common.errors import ReqFieldMissingError
+from decos import log
+
+# Инициализация клиентского логера
+LOGGER = logging.getLogger('client')
 
 
+@log
 def is_client_here(account_name='Guest'):
-    '''
+    """
     Функция генерирует запрос о присутствии клиента
     :param account_name:
     :return:
-    '''
+    """
     out = {
         ACTION: PRESENCE,
         TIME: time.time(),
@@ -22,47 +31,66 @@ def is_client_here(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
     return out
 
 
+@log
 def server_answer_in(message):
-    '''
+    """
     Функция разбирает ответ сервера
     :param message:
     :return:
-    '''
+    """
+    LOGGER.debug(f'Разбор сообщения от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
         return f'400 : {message[ERROR]}'
-    raise ValueError
+    raise ReqFieldMissingError(RESPONSE)
+
+
+@log
+def create_arg_parser():
+    """Создаём парсер аргументов коммандной строки"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    return parser
 
 
 def main():
-    '''Загружаем параметы коммандной строки'''
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+    """Загружаем параметы коммандной строки"""
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}. '
+            f'Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
+    LOGGER.info(f'Запущен клиент с парамертами: адрес сервера: '
+                f'{server_address}, порт: {server_port}')
     # Инициализация сокета и обмен
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = is_client_here()
-    message_out(transport, message_to_server)
     try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = is_client_here()
+        message_out(transport, message_to_server)
         answer = server_answer_in(message_in(transport))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+        LOGGER.info(f'Принят ответ от сервера {answer}')
+    except json.JSONDecodeError:
+        LOGGER.error('Не удалось декодировать полученную Json строку.')
+    except ReqFieldMissingError as missing_error:
+        LOGGER.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
+    except ConnectionRefusedError:
+        LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                        f'конечный компьютер отверг запрос на подключение.')
 
 
 if __name__ == '__main__':
