3cfa88ce1cf34d220d757f00ee08ef22e7857144
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-12-10 14:40:11 +0300

    Update variables.py

--- common/variables.py
+++ common/variables.py
@@ -1,3 +1,5 @@
+import logging
+
 """Константы"""
 
 # Порт поумолчанию для сетевого ваимодействия
@@ -10,15 +12,20 @@ MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
+# Текущий уровень логирования
+LOGGING_LEVEL = logging.DEBUG
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
 
 # Прочие ключи, используемые в протоколе
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
 RESPONDEFAULT_IP_ADDRESSSE = 'respondefault_ip_addressse'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
