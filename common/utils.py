0c1cabb05ba91ee4196b3b5e2983a2868d973217
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2021-12-24 16:52:43 +0300

    Add files via upload

--- common/utils.py
+++ common/utils.py
@@ -1,35 +1,33 @@
-"""Утилиты"""
-
+from common.variables import *
+from errors import IncorrectDataRecivedError, NonDictInputError
 import json
-from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+import sys
+sys.path.append('../')
+from decos import log
 
-def message_in(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
 
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
+@log
+def get_message(client):
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(ENCODING)
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        else:
+            raise IncorrectDataRecivedError
+    else:
+        raise IncorrectDataRecivedError
 
 
-def message_out(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
-
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
+@log
+def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
