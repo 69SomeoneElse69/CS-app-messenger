6d9c1f55ecfccc5d8a193f7685e766bacbf373a1
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2022-01-11 15:42:45 +0300

    Add files via upload

--- metaclasses.py
+++ metaclasses.py
@@ -1,52 +1,35 @@
-"""
-В основе метода библиотека dis - анализ кода с помощью его дизассемблирования
-(разбор кода на составляющие: в нашем случае - на атрибуты и методы класса)
-https://docs.python.org/3/library/dis.html
-"""
-
 import dis
 
 
 # Метакласс для проверки соответствия сервера:
 class ServerMaker(type):
     def __init__(cls, clsname, bases, clsdict):
-        # clsname - экземпляр метакласса - Server
-        # bases - кортеж базовых классов - ()
-        # clsdict - словарь атрибутов и методов экземпляра метакласса
-
         # Список методов, которые используются в функциях класса:
         methods = []
-        # Атрибуты, используемые в функциях классов
+        # Атрибуты, вызываемые функциями классов
         attrs = []
-        # перебираем ключи
         for func in clsdict:
+            # Пробуем
             try:
-                # Возвращает итератор по инструкциям в функции, методе, строке исходного кода или объекте кода.
                 ret = dis.get_instructions(clsdict[func])
                 # Если не функция то ловим исключение
-                # (если порт)
             except TypeError:
                 pass
             else:
-                # Если функция - получаем используемые методы и аргументы.
+                # Раз функция разбираем код, получая используемые методы и атрибуты.
                 for i in ret:
-                    # opname - имя для операции
                     if i.opname == 'LOAD_GLOBAL':
                         if i.argval not in methods:
-                            # заполняем список методами, использующимися в функциях класса
                             methods.append(i.argval)
                     elif i.opname == 'LOAD_ATTR':
                         if i.argval not in attrs:
-                            # заполняем список атрибутами, использующимися в функциях класса
                             attrs.append(i.argval)
-        print(methods)
-        # Если обнаружено использование недопустимого метода connect, вызываем исключение:
+        # Если обнаружено использование недопустимого метода connect, бросаем исключение:
         if 'connect' in methods:
             raise TypeError('Использование метода connect недопустимо в серверном классе')
         # Если сокет не инициализировался константами SOCK_STREAM(TCP) AF_INET(IPv4), тоже исключение.
         if not ('SOCK_STREAM' in attrs and 'AF_INET' in attrs):
             raise TypeError('Некорректная инициализация сокета.')
-        # Обязательно вызываем конструктор предка:
         super().__init__(clsname, bases, clsdict)
 
 
@@ -56,21 +39,22 @@ class ClientMaker(type):
         # Список методов, которые используются в функциях класса:
         methods = []
         for func in clsdict:
+            # Пробуем
             try:
                 ret = dis.get_instructions(clsdict[func])
-                # Пропускаем исключение если не функция
+                # Если не функция то ловим исключение
             except TypeError:
                 pass
             else:
-                # Если функция - получаем используемые методы.
+                # Раз функция разбираем код, получая используемые методы.
                 for i in ret:
                     if i.opname == 'LOAD_GLOBAL':
-                        if i.argval not in methods:  # Если нет в списке используемых методов
-                            methods.append(i.argval)  # Добавляем в список используемых методов
+                        if i.argval not in methods:
+                            methods.append(i.argval)
         # Если обнаружено использование недопустимого метода accept, listen, socket бросаем исключение:
         for command in ('accept', 'listen', 'socket'):
             if command in methods:
-                raise TypeError('В классе обнаружено использование недопустимого метода')
+                raise TypeError('В классе обнаружено использование запрещённого метода')
         # Вызов get_message или send_message из utils считаем корректным использованием сокетов
         if 'get_message' in methods or 'send_message' in methods:
             pass
