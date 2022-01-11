2201059db64095943fd4d77b4617c93f4dea9b3d
69SomeoneElse69 <73897654+69SomeoneElse69@users.noreply.github.com>
2022-01-11 21:25:56 +0300

    Add files via upload

--- common/utils.py
+++ common/utils.py
@@ -1,35 +1,26 @@
-"""Утилиты"""
-
 import json
-from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+import sys
+sys.path.append('../')
+from common.decos import log
+from common.variables import *
 
-def message_in(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
-
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку типа
+@log
+def get_message(client):
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode(ENCODING)
-        response = json.loads(json_response)
-        if isinstance(response, dict):
-            return response
-        raise ValueError
-    raise ValueError
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
 
 
-def message_out(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
-
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
+@log
+def send_message(sock, message):
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
